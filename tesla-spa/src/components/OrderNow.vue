9e4f877b4a4f74519b2febd19df4a5968a715a9a
9831065 <najafizadeh21@gmail.com>
2021-08-17 12:23:49 +0430

    Order now component added

---
+++ tesla-spa/src/components/OrderNow.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="background-color: black; margin-top: -10xp">
+    <div class="text-left pt-3">
+      <v-row>
+        <img
+          style="height: 600px"
+          src="https://tesla-cdn.thron.com/delivery/public/image/tesla/3d7892b2-7246-42e5-819a-cc119ede91b7/bvlatuR/std/1440x1080/MX-Specs-Hero-Desktop"
+        />
+        <v-layout align-center>
+            <h1 class="white--text ml-4">
+                Tesla Model X
+            </h1>
+            <v-btn style="border-radius:18px;" class="ml-3">
+                Order Now
+            </v-btn>
+        </v-layout>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
