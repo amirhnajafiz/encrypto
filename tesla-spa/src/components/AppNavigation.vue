ece629cfac9aa71c3e7a1522bf418fb4d506127d
9831065 <najafizadeh21@gmail.com>
2021-08-17 11:16:57 +0430

    Navbar added

---
+++ tesla-spa/src/components/AppNavigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>
+    <v-toolbar app color="black" dark>
+      <v-toolbar-title>
+        <v-img src="../../public/tesla.png" width="200">
+        </v-img>
+      </v-toolbar-title>
+      <v-btn flat class="hidden-sm-and-down white font-weight-bold black--text">
+        Specifications
+      </v-btn>
+      <v-btn
+        flat
+        class="hidden-sm-and-down white font-weight-bold black--text ml-3"
+      >
+        Order now
+      </v-btn>
+    </v-toolbar>
+  </span>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
